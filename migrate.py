f1ec8a6dbd282e67a171e768c16be6d1ff8648a6
suhan8984 <suhan8984@daum.net>
2024-09-17 18:53:17 +0900

    feat: migrate sitemap

--- migrate.py
+++ migrate.py
@@ -2,6 +2,7 @@ import os
 import json
 from bs4 import BeautifulSoup
 from datetime import datetime
+import urllib.parse
 
 def extract_title_from_html(file_path):
     """Extract title from an HTML file."""
@@ -42,10 +43,63 @@ def save_json(data, output_file):
     with open(output_file, 'w', encoding='utf-8') as file:
         json.dump(data, file, indent=4, ensure_ascii=False)
 
+def create_sitemap(data, output_file):
+    today = datetime.now().strftime("%Y-%m-%d")
+
+    # Main page
+    xml_content = f"""<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+        <loc>https://jsh6269.github.io/</loc>
+        <lastmod>{today}</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>0.8</priority>
+   </url>"""
+
+    # Guest page
+    xml_content += f"""
+    <url>
+        <loc>{urllib.parse.quote(f'https://jsh6269.github.io/guests/', safe=":/")}</loc>
+        <lastmod>{today}</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>0.6</priority>
+    </url>"""
+
+    # List page
+    for category in data.keys():
+        xml_content += f"""
+    <url>
+        <loc>{urllib.parse.quote(f'https://jsh6269.github.io/list/{category}/', safe=":/")}</loc>
+        <lastmod>{today}</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>0.6</priority>
+    </url>"""
+
+    # Post page
+    for category, post_list in data.items():
+        for post in post_list:
+            xml_content += f"""
+    <url>
+        <loc>{urllib.parse.quote(f'https://jsh6269.github.io/post/{category}/{post["id"]}/', safe=":/")}</loc>
+        <lastmod>{post['date']}</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>0.5</priority>
+    </url>"""
+    
+    xml_content += """
+</urlset>
+"""
+    with open(output_file, "w", encoding="utf-8") as file:
+        file.write(xml_content)
+
 if __name__ == "__main__":
-    base_directory = './public/posts'  # Base directory containing categories
-    output_file = './src/tree.json'        # Output JSON file
+    base_directory = './public/posts'
+    tree_json = './src/tree.json'
+    sitemap_file = 'public/sitemap.xml'
 
     data = create_json_from_html(base_directory)
-    save_json(data, output_file)
-    print(f"JSON file '{output_file}' created successfully.")
+    save_json(data, tree_json)
+    print(f"JSON file '{tree_json}' created successfully.")
+
+    create_sitemap(data, sitemap_file)
+    print(f"Sitemap file '{sitemap_file}' created successfully.")
